d1528e7f647e955e180c0a3c3711e7af5be074a9
cfloersch <chris.floersch@manheim.com>
2015-08-13 20:41:55 -0400

    Added tests and javadocs - null string throws null pointer rather than returning 0 - make class final

--- src/main/java/xpertss/time/Duration.java
+++ src/main/java/xpertss/time/Duration.java
@@ -12,23 +12,25 @@ import java.util.regex.Pattern;
  * This api supports source units of seconds (s), minutes (m), hours (h), and
  * days (d).
  */
-public class Duration {
+public final class Duration {
 
    private static final Pattern pattern = Pattern.compile("(\\d+)([smhd]{0,1})");
 
    /**
-    * Parse the given string duration into a value of the specified
-    * unit. If the source duration includes unit information the
-    * value will be converted to the specified time unit. Otherwise,
-    * the value is assumed to be in the specified time unit.
+    * Parse the given string duration into a value of the specified unit. If the
+    * source duration includes unit information the value will be converted to
+    * the specified time unit. Otherwise, the value is assumed to be in the
+    * specified time unit.
     *
     * @param str The source duration
     * @param unit The target time unit the source should be converted to
     * @return The duration converted to the specified unit
+    * @throws IllegalArgumentException if the specified string is invalid or negative
+    * @throws NullPointerException if teh specified unit or string are {@code null}
     */
    public static long parse(String str, TimeUnit unit)
    {
-      if(str == null) return 0L;
+      //if(str == null) return 0L;
       Matcher matcher = pattern.matcher(str);
       if(matcher.matches()) {
          long value = Long.parseLong(matcher.group(1));
--- src/test/java/xpertss/time/DurationTest.java
+++ src/test/java/xpertss/time/DurationTest.java
@@ -1,58 +1,114 @@
 package xpertss.time;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by cfloersch on 8/13/2015.
+ *
  */
 public class DurationTest {
 
    @Test
-   public void testNullString()
+   public void testBasicProof()
    {
-      assertEquals(0, Duration.parse(null, TimeUnit.SECONDS));
+      assertEquals(Duration.parse("1d", SECONDS), Duration.parse("24h", SECONDS));
+      assertEquals(Duration.parse("1440m", SECONDS), Duration.parse("24h", SECONDS));
+      assertEquals(Duration.parse("1440m", SECONDS), Duration.parse("86400s", SECONDS));
    }
+
+   @Test
+   public void testDaysToHours()
+   {
+      assertEquals(24L, Duration.parse("1d", HOURS));
+
+   }
+
+   @Test
+   public void testHoursToMinutes()
+   {
+      assertEquals(60L, Duration.parse("1h", MINUTES));
+
+   }
+
+   @Test
+   public void testMinutesToSeconds()
+   {
+      assertEquals(60L, Duration.parse("1m", SECONDS));
+
+   }
+
 
    @Test
    public void testZeroUnspecified()
    {
-      assertEquals(0, Duration.parse("0", TimeUnit.SECONDS));
+      assertEquals(0, Duration.parse("0", SECONDS));
    }
 
    @Test
    public void testZeroSeconds()
    {
-      assertEquals(0, Duration.parse("0s", TimeUnit.SECONDS));
+      assertEquals(0, Duration.parse("0s", SECONDS));
    }
 
    @Test
    public void testOneSeconds()
    {
-      assertEquals(1, Duration.parse("1s", TimeUnit.SECONDS));
+      assertEquals(1, Duration.parse("1s", SECONDS));
    }
 
    @Test
    public void testOneMinute()
    {
-      assertEquals(60, Duration.parse("1m", TimeUnit.SECONDS));
+      assertEquals(60, Duration.parse("1m", SECONDS));
    }
 
    @Test
    public void testOneHour()
    {
-      assertEquals(3600, Duration.parse("1h", TimeUnit.SECONDS));
+      assertEquals(3600, Duration.parse("1h", SECONDS));
    }
 
    @Test
    public void testOneDay()
    {
-      assertEquals(3600 * 24, Duration.parse("1d", TimeUnit.SECONDS));
+      assertEquals(3600 * 24, Duration.parse("1d", SECONDS));
    }
 
 
+
+   @Test(expected = NullPointerException.class)
+   public void testNullString()
+   {
+      Duration.parse(null, SECONDS);
+   }
+
+   @Test(expected = NullPointerException.class)
+   public void testNullUnit()
+   {
+      Duration.parse("10s", null);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void testInvalidUnitChars()
+   {
+      Duration.parse("100dd", SECONDS);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void testInvalidDigits()
+   {
+      Duration.parse("3d3s", SECONDS);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void testNegativeDuration()
+   {
+      Duration.parse("-3d", SECONDS);
+   }
 }
