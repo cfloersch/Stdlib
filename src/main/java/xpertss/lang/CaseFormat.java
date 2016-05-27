d9988e844dc08b6a0409ae0dff8805cc39dbae15
cfloersch <chris.floersch@manheim.com>
2016-05-27 19:05:54 -0400

    Add case format capability

---
+++ src/main/java/xpertss/lang/CaseFormat.java
@@ -0,0 +1,225 @@
+/**
+ * Copyright 2016 XpertSoftware
+ * <p/>
+ * Created By: cfloersch
+ * Date: 5/27/2016
+ */
+package xpertss.lang;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum CaseFormat {
+
+
+   /**
+    * Hyphenated variable naming convention, e.g., "lower-hyphen".
+    */
+   LOWER_HYPHEN() {
+      String create(String[] parts)
+      {
+         StringBuilder builder = new StringBuilder();
+         for(String part : parts) {
+            if(builder.length() > 0) builder.append("-");
+            builder.append(part);
+         }
+         return builder.toString();
+      }
+      boolean matches(String str)
+      {
+         boolean foundHyphen = false;
+         for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(Character.isUpperCase(c)) return false;
+            if(c == '_') return false;
+            if(c == '-') foundHyphen = true;
+         }
+         return foundHyphen;
+      }
+      String[] parts(String str)
+      {
+         return str.toLowerCase().split("-");
+      }
+   },
+
+   /**
+    * C++ variable naming convention, e.g., "lower_underscore".
+    */
+   LOWER_UNDERSCORE() {
+      String create(String[] parts)
+      {
+         StringBuilder builder = new StringBuilder();
+         for(String part : parts) {
+            if(builder.length() > 0) builder.append("_");
+            builder.append(part);
+         }
+         return builder.toString();
+      }
+      boolean matches(String str)
+      {
+         boolean foundUnderscore = false;
+         for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(Character.isUpperCase(c)) return false;
+            if(c == '-') return false;
+            if(c == '_') foundUnderscore = true;
+         }
+         return foundUnderscore;
+      }
+      String[] parts(String str)
+      {
+         return str.toLowerCase().split("_");
+      }
+   },
+
+   /**
+    * Java variable naming convention, e.g., "lowerCamel".
+    */
+   LOWER_CAMEL() {
+      String create(String[] parts)
+      {
+         StringBuilder builder = new StringBuilder();
+         for(String part : parts) {
+            if(builder.length() > 0) {
+               builder.append(Strings.firstCharToUpper(part));
+            } else {
+               builder.append(part);
+            }
+         }
+         return builder.toString();
+      }
+      boolean matches(String str)
+      {
+         boolean firstIsLower = false;
+         boolean foundUpper = false;
+         for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(i == 0 && Character.isLowerCase(c)) firstIsLower = true;
+            if(Character.isUpperCase(c)) foundUpper = true;
+            if(c == '-') return false;
+            if(c == '_') return false;
+         }
+         return firstIsLower && foundUpper;
+      }
+   },
+
+   /**
+    * Java and C++ class naming convention, e.g., "UpperCamel".
+    */
+   UPPER_CAMEL() {
+      String create(String[] parts)
+      {
+         StringBuilder builder = new StringBuilder();
+         for(String part : parts) {
+            builder.append(Strings.firstCharToUpper(part));
+         }
+         return builder.toString();
+      }
+      boolean matches(String str)
+      {
+         boolean firstIsUpper = false;
+         boolean foundUpper = false;
+         boolean foundLower = false;
+         for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(i == 0 && Character.isUpperCase(c)) firstIsUpper = true;
+            if(Character.isUpperCase(c)) foundUpper = true;
+            if(Character.isLowerCase(c)) foundLower = true;
+            if(c == '-') return false;
+            if(c == '_') return false;
+         }
+         return firstIsUpper && foundUpper && foundLower;
+      }
+   },
+
+   /**
+    * Java and C++ constant naming convention, e.g., "UPPER_UNDERSCORE".
+    */
+   UPPER_UNDERSCORE() {
+      String create(String[] parts)
+      {
+         StringBuilder builder = new StringBuilder();
+         for(String part : parts) {
+            if(builder.length() > 0) builder.append("_");
+            builder.append(part.toUpperCase());
+         }
+         return builder.toString();
+      }
+      boolean matches(String str)
+      {
+         boolean foundUnderscore = false;
+         for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(Character.isLowerCase(c)) return false;
+            if(c == '-') return false;
+            if(c == '_') foundUnderscore = true;
+         }
+         return foundUnderscore;
+      }
+      String[] parts(String str)
+      {
+         return str.toLowerCase().split("_");
+      }
+   };
+
+
+
+
+
+
+
+   /**
+    * Converts the specified {@code String str} from this format to the specified {@code format}. A
+    * "best effort" approach is taken; if {@code str} does not conform to the assumed format, then
+    * the behavior of this method is undefined but we make a reasonable effort at converting anyway.
+    */
+   public final String to(CaseFormat format, String str)
+   {
+      return (format == this) ? str : format.create(parts(str));
+   }
+
+
+   /**
+    * Attempts to determine the case of the string. This will return the CaseFormat for the
+    * string if it can be determined, or {@code null} otherwise.
+    */
+   public static CaseFormat forString(String str)
+   {
+      for(CaseFormat format : values()) {
+         if(format.matches(str)) return format;
+      }
+      return null;
+   }
+
+
+
+   String create(String[] parts)
+   {
+      return null;
+   }
+
+   boolean matches(String str)
+   {
+      return false;
+   }
+
+   String[] parts(String str)
+   {
+      StringBuilder builder = new StringBuilder();
+      List<String> parts = new ArrayList<>();
+      for(int i = 0; i < str.length(); i++) {
+         char c = str.charAt(i);
+         if(Character.isUpperCase(c)) {
+            parts.add(builder.toString().toLowerCase());
+            builder.setLength(0);
+         }
+         builder.append(c);
+      }
+      parts.add(builder.toString().toLowerCase());
+      return parts.toArray(new String[parts.size()]);
+   }
+
+
+
+
+}
---
+++ src/test/java/xpertss/lang/CaseFormatTest.java
@@ -0,0 +1,186 @@
+package xpertss.lang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static xpertss.lang.CaseFormat.*;
+
+/**
+ * Copyright 2016 XpertSoftware
+ * <p/>
+ * Created By: cfloersch
+ * Date: 5/27/2016
+ */
+public class CaseFormatTest {
+
+
+   @Test
+   public void testForStringUpperUnderScore()
+   {
+      assertEquals(UPPER_UNDERSCORE, forString("HELLO_KITTY"));
+   }
+
+   @Test
+   public void testForStringLowerUnderScore()
+   {
+      assertEquals(LOWER_UNDERSCORE, forString("hello_kitty"));
+   }
+
+   @Test
+   public void testForStringLowerHyphen()
+   {
+      assertEquals(LOWER_HYPHEN, forString("hello-kitty"));
+   }
+
+   @Test
+   public void testForStringLowerCamel()
+   {
+      assertEquals(LOWER_CAMEL, forString("helloKitty"));
+   }
+
+   @Test
+   public void testForStringUpperCamel()
+   {
+      assertEquals(UPPER_CAMEL, forString("HelloKitty"));
+   }
+
+   @Test
+   public void testForSingleWord()
+   {
+      assertNull(forString("hello"));
+   }
+
+
+   @Test
+   public void testUpperUnderscoreToUpperCamel()
+   {
+      assertEquals("HelloKitty", UPPER_UNDERSCORE.to(UPPER_CAMEL, "HELLO_KITTY"));
+   }
+
+   @Test
+   public void testUpperUnderscoreToLowerCamel()
+   {
+      assertEquals("helloKitty", UPPER_UNDERSCORE.to(LOWER_CAMEL, "HELLO_KITTY"));
+   }
+
+   @Test
+   public void testUpperUnderscoreToLowerUnderscore()
+   {
+      assertEquals("hello_kitty", UPPER_UNDERSCORE.to(LOWER_UNDERSCORE, "HELLO_KITTY"));
+   }
+
+   @Test
+   public void testUpperUnderscoreToLowerHyphen()
+   {
+      assertEquals("hello-kitty", UPPER_UNDERSCORE.to(LOWER_HYPHEN, "HELLO_KITTY"));
+   }
+
+
+
+   @Test
+   public void testUpperCamelToUpperUnderscore()
+   {
+      assertEquals("HELLO_KITTY", UPPER_CAMEL.to(UPPER_UNDERSCORE, "HelloKitty"));
+   }
+
+   @Test
+   public void testUpperCamelToLowerCamel()
+   {
+      assertEquals("helloKitty", UPPER_CAMEL.to(LOWER_CAMEL, "HelloKitty"));
+   }
+
+   @Test
+   public void testUpperCamelToLowerUnderscore()
+   {
+      assertEquals("hello_kitty", UPPER_CAMEL.to(LOWER_UNDERSCORE, "HelloKitty"));
+   }
+
+   @Test
+   public void testUpperCamelToLowerHyphen()
+   {
+      assertEquals("hello-kitty", UPPER_CAMEL.to(LOWER_HYPHEN, "HelloKitty"));
+   }
+
+
+
+   @Test
+   public void testLowerCamelToUpperUnderscore()
+   {
+      assertEquals("HELLO_KITTY", LOWER_CAMEL.to(UPPER_UNDERSCORE, "helloKitty"));
+   }
+
+   @Test
+   public void testLowerCamelToUpperCamel()
+   {
+      assertEquals("HelloKitty", LOWER_CAMEL.to(UPPER_CAMEL, "helloKitty"));
+   }
+
+   @Test
+   public void testLowerCamelToLowerUnderscore()
+   {
+      assertEquals("hello_kitty", LOWER_CAMEL.to(LOWER_UNDERSCORE, "helloKitty"));
+   }
+
+   @Test
+   public void testLowerCamelToLowerHyphen()
+   {
+      assertEquals("hello-kitty", LOWER_CAMEL.to(LOWER_HYPHEN, "helloKitty"));
+   }
+
+
+
+
+   @Test
+   public void testLowerHyphenToUpperUnderscore()
+   {
+      assertEquals("HELLO_KITTY", LOWER_HYPHEN.to(UPPER_UNDERSCORE, "hello-kitty"));
+   }
+
+   @Test
+   public void testLowerHyphenToUpperCamel()
+   {
+      assertEquals("HelloKitty", LOWER_HYPHEN.to(UPPER_CAMEL, "hello-kitty"));
+   }
+
+   @Test
+   public void testLowerHyphenToLowerCamel()
+   {
+      assertEquals("helloKitty", LOWER_HYPHEN.to(LOWER_CAMEL, "hello-kitty"));
+   }
+
+   @Test
+   public void testLowerHyphenToLowerUnderscore()
+   {
+      assertEquals("hello_kitty", LOWER_HYPHEN.to(LOWER_UNDERSCORE, "hello-kitty"));
+   }
+
+
+
+
+   @Test
+   public void testLowerUnderscoreToUpperUnderscore()
+   {
+      assertEquals("HELLO_KITTY", LOWER_UNDERSCORE.to(UPPER_UNDERSCORE, "hello_kitty"));
+   }
+
+   @Test
+   public void testLowerUnderscoreToUpperCamel()
+   {
+      assertEquals("HelloKitty", LOWER_UNDERSCORE.to(UPPER_CAMEL, "hello_kitty"));
+   }
+
+   @Test
+   public void testLowerUnderscoreToLowerCamel()
+   {
+      assertEquals("helloKitty", LOWER_UNDERSCORE.to(LOWER_CAMEL, "hello_kitty"));
+   }
+
+   @Test
+   public void testLowerUnderscoreToLowerHyphen()
+   {
+      assertEquals("hello-kitty", LOWER_UNDERSCORE.to(LOWER_HYPHEN, "hello_kitty"));
+   }
+
+
+
+}
