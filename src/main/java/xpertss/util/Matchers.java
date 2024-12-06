f64f6c75f6b73ff7ec2bc8ebe8e3915c39a7b8db
cfloersch <chris.floersch@coxautoinc.com>
2024-12-06 12:29:50 -0500

    Add javadocs

--- src/main/java/xpertss/util/Matchers.java
+++ src/main/java/xpertss/util/Matchers.java
@@ -8,10 +8,20 @@ package xpertss.util;
 
 import java.util.regex.Matcher;
 
+/**
+ * Simple Regex Matcher utility functions.
+ */
 public final class Matchers {
 
    private Matchers() { }
 
+   /**
+    * Converts the groups associated with the given matcher into an array
+    * excluding group zero.
+    *
+    * @param matcher The matcher from which groups are pulled.
+    * @return An array of group values excluding group 0
+    */
    public static String[] groups(Matcher matcher)
    {
       String[] result = new String[matcher.groupCount()];
