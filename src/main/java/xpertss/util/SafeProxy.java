6fe53c771de43a59b1c6a22a6b8244d33988fc69
Chris Floersch <chris.floersch@coxautoinc.com>
2024-12-16 17:19:05 -0500

    New SafeProxy class

---
+++ src/main/java/xpertss/util/SafeProxy.java
@@ -0,0 +1,68 @@
+package xpertss.util;
+
+import xpertss.threads.Threads;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * This class will wrap a given class and proxy calls to it catching any runtime
+ * exceptions that may be thrown. Those exceptions will be sent to the calling
+ * thread's uncaught exception handler and null will be returned.
+ *
+ * @param <T>
+ */
+public class SafeProxy <T> implements InvocationHandler {
+
+    /**
+     * Construct an instance of the SafeDispatcher compatible with the given listener class.
+     */
+    public static <T> T newInstance(Class<T> proxiedClass, T proxied)
+    {
+        return proxiedClass.cast(Proxy.newProxyInstance(proxiedClass.getClassLoader(),
+                new Class[] { proxiedClass },
+                new SafeProxy<T>(proxied)));
+    }
+
+
+    private final T proxied;
+
+
+    private SafeProxy(T proxied)
+    {
+        this.proxied = proxied;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, final Method method, final Object[] args)
+            throws Throwable
+    {
+        if ("equals".equals(method.getName())) {
+            return (args[0] == proxy);
+        } else if("hashCode".equals(method.getName())) {
+            return hashCode();
+        } else if("toString".equals(method.getName())) {
+            return toString();
+        } else {
+            try {
+                return method.invoke(proxied, args);
+            } catch(InvocationTargetException e) {
+                Threads.report(e.getTargetException());
+            } catch(Throwable t) {
+                Threads.report(t);
+            }
+            return null;
+        }
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "SafeProxy<" + proxied.toString() + ">";
+    }
+
+}
